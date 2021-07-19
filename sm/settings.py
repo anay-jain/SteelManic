f9181c26e99de67035750a9d67d872068940af50
Anay Jain <anay@Anays-MacBook-Pro.local>
2021-07-19 19:42:06 +0530

    changed settings fike

--- sm/settings.py
+++ sm/settings.py
@@ -37,8 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'mainview.apps.MainviewConfig',
-    'contactus',
+    'mainview.apps.MainviewConfig'
 ]
 
 MIDDLEWARE = [
@@ -121,7 +120,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT =os.path.join(BASE_DIR,'media')
